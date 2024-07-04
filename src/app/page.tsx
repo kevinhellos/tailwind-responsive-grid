47c427e877bdfbb1b0dd061578d2c56564886840
Kevin <kevin@eaucole.com>
2024-07-05 02:39:34 +0800

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,8 @@ export default function page() {
     <div>
 
       <h1 className="px-5 mt-5">Grid - 4 columns on <span className="font-medium">all screen sizes & viewport</span></h1>
-      <p className="px-5 mb-10 w-fit bg-gray-100">grid grid-cols-4</p>
+      <span className="ms-5 text-xs">Class:</span>
+      <p className="ms-5 px-5 mb-10 w-fit bg-gray-100">grid grid-cols-4</p>
       <div className="grid grid-cols-4">
         <div className="bg-red-500">Hello</div>
         <div className="bg-green-500">Hello</div>
@@ -17,7 +18,8 @@ export default function page() {
         Grid - 4 columns on <span className="font-medium text-red-500">large screens</span> <br />
         Grid - 2 columns on <span className="font-medium text-blue-500">medium screens</span>
       </h1>
-      <p className="px-5 mb-10 w-fit bg-gray-100">grid <span className="text-blue-500">md</span>:grid-cols-2 <span className="text-red-500">lg</span>:grid-cols-4</p>
+      <span className="ms-5 text-xs">Class:</span>
+      <p className="ms-5 px-5 mb-10 w-fit bg-gray-100">grid <span className="text-blue-500">md</span>:grid-cols-2 <span className="text-red-500">lg</span>:grid-cols-4</p>
       <div className="grid md:grid-cols-2 lg:grid-cols-4">
         <div className="bg-red-500">Hello</div>
         <div className="bg-green-500">Hello</div>
@@ -29,7 +31,8 @@ export default function page() {
         Grid - 6 columns on <span className="font-medium text-red-500">large screens</span> <br />
         Grid - 3 columns on <span className="font-medium text-blue-500">medium screens</span>
       </h1>
-      <p className="px-5 mb-10 w-fit bg-gray-100">grid <span className="text-blue-500">md</span>:grid-cols-3 <span className="text-red-500">lg</span>:grid-cols-6</p>
+      <span className="ms-5 text-xs">Class:</span>
+      <p className="ms-5 px-5 mb-10 w-fit bg-gray-100">grid <span className="text-blue-500">md</span>:grid-cols-3 <span className="text-red-500">lg</span>:grid-cols-6</p>
       <div className="grid md:grid-cols-3 lg:grid-cols-6">
         <div className="bg-red-500">Hello</div>
         <div className="bg-green-500">Hello</div>
